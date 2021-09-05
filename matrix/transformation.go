cb5dd6f193e5f3e1457a50c48059181ba6fad2d4
Daniel Szatmari <daniel.kaizencodes@gmail.com>
2021-09-05 15:48:22 +0200

    implement scaling

--- matrix/transformation.go
+++ matrix/transformation.go
@@ -24,11 +24,39 @@ func Translate(t Transformable, x, y, z float64) (Transformable, error) {
 	}
 }
 
+func Scale(t Transformable, x, y, z float64) (Transformable, error) {
+	switch t := t.(type) {
+	case tuple.Tuple:
+		mat := New(4, 1)
+		for i, v := range t.ToSlice() {
+			mat[i][0] = float64(v)
+		}
+		mat, err := multiply_matrices(scaling_matrix(x, y, z), mat)
+		if err != nil {
+			return nil, err
+		}
+		mat = mat.Transpose()
+		return tuple.Tuple{mat[0][0], mat[0][1], mat[0][2], mat[0][3]}, nil
+
+	default:
+		return nil, fmt.Errorf("incompatible type for matrix multiplication: %T", t)
+	}
+}
+
 func translation_matrix(x, y, z float64) Matrix {
 	return Matrix{
 		[]float64{1, 0, 0, x},
 		[]float64{0, 1, 0, y},
 		[]float64{0, 0, 1, z},
+		[]float64{0, 0, 0, 1},
+	}
+}
+
+func scaling_matrix(x, y, z float64) Matrix {
+	return Matrix{
+		[]float64{x, 0, 0, 0},
+		[]float64{0, y, 0, 0},
+		[]float64{0, 0, z, 0},
 		[]float64{0, 0, 0, 1},
 	}
 }
--- matrix/transformation_test.go
+++ matrix/transformation_test.go
@@ -29,3 +29,29 @@ func TestTranslate(t *testing.T) {
     }
 
 }
+
+func TestScale(t *testing.T) {
+    point := tuple.Tuple{-4, 6, 8, 1}
+    want := tuple.Tuple{-8, 18, 32, 1}
+    var x, y, z float64
+    x, y, z = 2, 3, 4
+    if got, _ := Scale(point, x, y, z); got.String() != want.String() {
+        t.Errorf("scaling(%f, %f, %f),\na:\n%s\n\ngot:\n%s\nexpected: \n%s", x, y, z, point, got, want)
+    }
+
+    inv, err := scaling_matrix(x, y, z).Inverse()
+    if err != nil {
+        t.Error(err)
+    }
+    want = tuple.Tuple{-2, 2, 2, 1}
+    if got, _ := Multiply(inv, point); got.String() != want.String() {
+        t.Errorf("inverse scaling(%f, %f, %f),\na:\n%s\n\ngot:\n%s\nexpected: \n%s", x, y, z, point, got, want)
+    }
+
+    vector := tuple.Tuple{-4, 6, 8, 0}
+    want = tuple.Tuple{-8, 18, 32, 0}
+    if got, _ := Scale(vector, x, y, z); got.String() != want.String() {
+        t.Errorf("vector scaling(%f, %f, %f),\na:\n%s\n\ngot:\n%s", x, y, z, vector, got)
+    }
+
+}
