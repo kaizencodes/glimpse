cecbf9c063b9b98e55e172efc23a74cf32b488e9
Daniel Szatmari <daniel.kaizencodes@gmail.com>
2021-09-07 09:19:47 +0200

    hide internal tuple variables

--- ray/ray_test.go
+++ ray/ray_test.go
@@ -12,24 +12,24 @@ func TestPosition(t *testing.T) {
         want tuple.Tuple
     }{
         {
-            ray:  New(tuple.Tuple{2, 3, 4, 1}, tuple.Tuple{1, 0, 0, 0}),
+            ray:  New(tuple.NewPoint(2, 3, 4), tuple.NewVector(1, 0, 0)),
             dist: 0,
-            want: tuple.Tuple{2, 3, 4, 1},
+            want: tuple.NewPoint(2, 3, 4),
         },
         {
-            ray:  New(tuple.Tuple{2, 3, 4, 1}, tuple.Tuple{1, 0, 0, 0}),
+            ray:  New(tuple.NewPoint(2, 3, 4), tuple.NewVector(1, 0, 0)),
             dist: 1,
-            want: tuple.Tuple{3, 3, 4, 1},
+            want: tuple.NewPoint(3, 3, 4),
         },
         {
-            ray:  New(tuple.Tuple{2, 3, 4, 1}, tuple.Tuple{0, 1, 0, 0}),
+            ray:  New(tuple.NewPoint(2, 3, 4), tuple.NewVector(0, 1, 0)),
             dist: -1,
-            want: tuple.Tuple{2, 2, 4, 1},
+            want: tuple.NewPoint(2, 2, 4),
         },
         {
-            ray:  New(tuple.Tuple{2, 3, 4, 1}, tuple.Tuple{0, 0, 1, 0}),
+            ray:  New(tuple.NewPoint(2, 3, 4), tuple.NewVector(0, 0, 1)),
             dist: 2.5,
-            want: tuple.Tuple{2, 3, 6.5, 1},
+            want: tuple.NewPoint(2, 3, 6.5),
         },
     }
 
