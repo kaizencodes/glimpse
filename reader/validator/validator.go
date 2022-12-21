ce79777f0659dd341bbce67a7ff2c1fbe0def2d4
Daniel Szatmari <daniel.kaizencodes@gmail.com>
2022-12-21 15:43:09 +0100

    fix relative path issue
    the file reader uses the pwd and pwd changes for every test file so we need to get the project folder to consistently load the schema file.

--- reader/validator/validator.go
+++ reader/validator/validator.go
@@ -1,100 +1,20 @@
 package validator
 
 import (
+	"glimpse/projectpath"
 	"os"
 
 	"cuelang.org/go/cue/cuecontext"
 	"cuelang.org/go/encoding/yaml"
 )
 
-const schemaPath = "./schema.cue"
-
-// const schema = `#Tuple: 3 * [number]
-
-// #Camera: {
-// 	width: number
-// 	height: number
-// 	fov: number
-// 	from: #Tuple
-//   to: #Tuple
-//   up: #Tuple
-// }
-// #Light: {
-//   position: #Tuple
-//   intensity: #Tuple
-// }
-
-// #scale: ["scale", number, number, number]
-// #translate: ["translate", number, number, number]
-// #rotateX: ["rotateX", number]
-// #rotateY: ["rotateY", number]
-// #rotateZ: ["rotateZ", number]
-
-// #transform: [...#scale | #translate | #rotateX | #rotateY | #rotateZ]
-
-// #material: {
-//     ambient: number
-//     diffuse: number
-//     specular: number
-//     shininnes: number
-//     reflective: number
-//     transparency: number
-//     refractiveIndex: number
-// }
-
-// #Sphere: {
-//   sphere: {
-//     transform: #transform
-//     material?: #material
-//   }
-// }
-
-// #Cube: {
-//   cube: {
-//     transform: #transform
-//     material?: #material
-//   }
-// }
-
-// #Plane: {
-//   plane: {
-//     transform: #transform
-//     material?: #material
-//   }
-// }
-
-// #Cylinder: {
-//   cylinder: {
-//     transform: #transform
-//     minimum: number
-//     maximum: number
-//     closed: bool
-//     material?: #material
-
-//   }
-// }
-
-// #Model: {
-//   model: {
-//     path: string
-//     transform?: #transform
-//   }
-// }
-
-// #Shapes: {
-// [...#Sphere | #Cube | #Plane | #Cylinder | #Model]
-// }
-
-// camera: #Camera
-// light: #Light
-// shapes: #Shapes
-// `
+// relative path to the project root.
+const schemaPath = "/reader/validator/schema.cue"
 
 // Validates the config file against the schema using cue.
-
 func Validate(config []byte) error {
 	ctx := cuecontext.New()
-	schema, err := os.ReadFile(schemaPath)
+	schema, err := os.ReadFile(projectpath.Root + schemaPath)
 	if err != nil {
 		panic(err)
 	}
